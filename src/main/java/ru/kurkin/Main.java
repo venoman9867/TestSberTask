0b6b3afa39d0fcb634666e905992b43928d91ee7
Alexsandr <venoman9867@gmail.com>
2023-10-18 20:06:21 +0300

    Пятое задание

--- src/main/java/ru/kurkin/Main.java
+++ src/main/java/ru/kurkin/Main.java
@@ -4,30 +4,34 @@ import ru.kurkin.model.City;
 
 import java.io.File;
 import java.io.FileNotFoundException;
-import java.util.Arrays;
-import java.util.LinkedList;
-import java.util.List;
-import java.util.Scanner;
+import java.util.*;
 
 public class Main {
     public static void main(String[] args) throws FileNotFoundException {
-        List<City> cityList = new LinkedList<>();//Использую линкед потому что в него вставка быстрее чем в arrayList
+        Map<String, List<City>> cityInDistrict = new HashMap<>();
         Scanner scanner = new Scanner(new File("src/main/resources/Задача ВС Java Сбер.csv"));
         while (scanner.hasNext()) {
             String[] strings = scanner.nextLine().split(";");
+            //242 строка без 6 значения??????
             if (strings.length < 6) {
                 strings = Arrays.copyOf(strings, 6);
                 strings[5] = null;
             }
             City city = new City(
-                    Integer.parseInt(strings[0]),
-                    strings[1],
-                    strings[2],
-                    strings[3],
-                    Integer.parseInt(strings[4]),
-                    strings[5]);
-            cityList.add(city);
+                    Integer.parseInt(strings[0]),//id
+                    strings[1],//название города
+                    strings[2],//регион
+                    strings[3],//федеральный округ
+                    Integer.parseInt(strings[4]),//популяция
+                    strings[5]);//дата основания
+            var cities = cityInDistrict.getOrDefault(city.getRegion(), new ArrayList<>());
+            cities.add(city);
+            cityInDistrict.put(city.getRegion(), cities);
         }
-        cityList.forEach(System.out::println);
+        for (var entry : cityInDistrict.entrySet()) {
+            var region = entry.getKey();
+            var cities = entry.getValue();
+            System.out.printf("%s - %s%n" , region, cities.size());
+        }
     }
 }
--- src/main/java/ru/kurkin/model/City.java
+++ src/main/java/ru/kurkin/model/City.java
@@ -1,6 +1,8 @@
 package ru.kurkin.model;
 
-public class City{
+import java.util.Objects;
+
+public class City {
     private int id;
     private String name;
     private String region;
@@ -17,13 +19,55 @@ public class City{
         this.foundation = foundation;
     }
 
+    public String getDistrict() {
+        return district;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getRegion() {
+        return region;
+    }
+
+    public int getPopulation() {
+        return population;
+    }
+
+    public String getFoundation() {
+        return foundation;
+    }
+
     public String getName() {
         return name;
     }
 
     @Override
     public String toString() {
-        return "City{name='"+name+"', region='"+region+"', district='"+district+"', population="+population+", foundation='"+foundation+"'}";
+        return "City{name='" + name + "', region='" + region + "', district='" + district + "', population=" + population + ", foundation='" + foundation + "'}";
     }
 
+
+    @Override
+    public boolean equals(Object obj) {
+        if (obj == this) {
+            return true;
+        }
+        if (obj == null || obj.getClass() != this.getClass()) {
+            return false;
+        }
+        City city = (City) obj;
+        //Численные типы проверяются быстрее всего
+        return id == city.getId() && population == city.getPopulation()
+                && (name == city.getName() || name != null && name.equals(city.getName()))
+                && (region == city.getRegion() || region != null && region.equals(city.getRegion()))
+                && (district == city.getDistrict() || district != null && district.equals(city.getDistrict()))
+                && (foundation == city.getFoundation() || foundation != null && foundation.equals(city.getFoundation()));
+    }
+    @Override
+    public int hashCode() {
+        return Objects.hash(id,name,region,district,population,foundation);
+    }
 }
+
