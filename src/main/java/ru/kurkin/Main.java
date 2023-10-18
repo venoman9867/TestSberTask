765f092c697f0cfcbf203922feafcb67cd392a26
Alexsandr <venoman9867@gmail.com>
2023-10-18 20:13:15 +0300

    Поменял на var типы

--- src/main/java/ru/kurkin/Main.java
+++ src/main/java/ru/kurkin/Main.java
@@ -8,30 +8,30 @@ import java.util.*;
 
 public class Main {
     public static void main(String[] args) throws FileNotFoundException {
-        Map<String, List<City>> cityInDistrict = new HashMap<>();
-        Scanner scanner = new Scanner(new File("src/main/resources/Задача ВС Java Сбер.csv"));
+        var cityInDistrict = new HashMap<String, Set<City>>();
+        var scanner = new Scanner(new File("src/main/resources/Задача ВС Java Сбер.csv"));
         while (scanner.hasNext()) {
-            String[] strings = scanner.nextLine().split(";");
+            var strings = scanner.nextLine().split(";");
             //242 строка без 6 значения??????
             if (strings.length < 6) {
                 strings = Arrays.copyOf(strings, 6);
                 strings[5] = null;
             }
-            City city = new City(
+            var city = new City(
                     Integer.parseInt(strings[0]),//id
                     strings[1],//название города
                     strings[2],//регион
                     strings[3],//федеральный округ
                     Integer.parseInt(strings[4]),//популяция
                     strings[5]);//дата основания
-            var cities = cityInDistrict.getOrDefault(city.getRegion(), new ArrayList<>());
+            var cities = cityInDistrict.getOrDefault(city.getRegion(), new HashSet<>());
             cities.add(city);
             cityInDistrict.put(city.getRegion(), cities);
         }
         for (var entry : cityInDistrict.entrySet()) {
             var region = entry.getKey();
             var cities = entry.getValue();
-            System.out.printf("%s - %s%n" , region, cities.size());
+            System.out.printf("%s - %s%n", region, cities.size());
         }
     }
 }
